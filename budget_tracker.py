3cbaab3453ea35ed8d413af6bdfe4a50e96b04bb
Kairung Vangmanaw <94585323+VangmanawKairung@users.noreply.github.com>
2025-08-20 14:45:50 +0700

    Update budget_tracker.py

--- budget_tracker.py
+++ budget_tracker.py
@@ -14,9 +14,12 @@ APP_TITLE = "Budget Tracker"
 APP_WIDTH = 1000
 APP_HEIGHT = 800
 DISPLAY_PANEL_WIDTH = APP_WIDTH * 0.7
-CONTROL_PANEL_WIDTH = APP_WIDTH - DISPLAY_PANEL_WIDTH
-BG_COLOR = "#212121"  # Background color
+INPUT_CONTROL_PANEL_WIDTH = APP_WIDTH * 0.3
+PADDING = 20
+BG_COLOR = "#ff0000"  # Background color
 FG_COLOR = "#faf9f6"  # Foreground (text) color
+CONTROL_BG_COLOR = "#121212"  # Control panel background color
+DISPLAY_BG_COLOR = "#171717"  # Display panel background color
 
 # CSV file for persistent storage
 CSV_FILE = "transactions.csv"
@@ -32,6 +35,8 @@ class BudgetTracker:
         x = (screen_width // 2) - (APP_WIDTH // 2)
         y = (screen_height // 2) - (APP_HEIGHT // 2)
         self.root.geometry(f"{APP_WIDTH}x{APP_HEIGHT}+{x}+{y}")
+        self.root.resizable(False, False)
+        self.root.configure(bg=BG_COLOR)
 
         # Variables
         self.date_var = StringVar(value=datetime.now().strftime("%d-%m-%Y"))
@@ -46,14 +51,55 @@ class BudgetTracker:
     def create_widget(self):
         # กำหนด weight ให้ root เพื่อให้แต่ละคอลัมน์ขยายตามขนาดหน้าต่าง
         self.root.grid_rowconfigure(0, weight=1)
-        self.root.grid_columnconfigure(0, weight=7)  # display_panel (70%)
-        self.root.grid_columnconfigure(1, weight=3)  # control_panel (30%)
+        self.root.grid_columnconfigure(0, weight=1)
+        self.root.grid_columnconfigure(1, weight=1)
 
-        display_panel = Frame(self.root, bg="#FF0000")
-        display_panel.grid(row=0, column=0, sticky="nsew", pady=10, padx=10)
+        # Create display panel
+        display_panel = Frame(self.root, bg=DISPLAY_BG_COLOR, width=DISPLAY_PANEL_WIDTH, height=APP_HEIGHT)
+        display_panel.grid(row=0, column=0, sticky="nsew", pady=PADDING, padx=PADDING)
+        display_panel.grid_propagate(False)
 
-        control_panel = Frame(self.root, bg="#00FF00")
-        control_panel.grid(row=0, column=1, sticky="nsew")
+        # Create input and control panel
+        input_control_panel = Frame(self.root, bg=CONTROL_BG_COLOR, width=INPUT_CONTROL_PANEL_WIDTH, height=APP_HEIGHT)
+        input_control_panel.grid(row=0, column=1, sticky="nsew")
+        input_control_panel.grid_propagate(False)
+        
+        # Configure grid for control panel
+        for i in range(2):
+            input_control_panel.grid_rowconfigure(i, weight=1)
+        for i in range(1):
+            input_control_panel.grid_columnconfigure(i, weight=1)
+        
+        # Create input panel inside input and control panel
+        input_panel = Frame(input_control_panel, bg="#00ff00")
+        input_panel.grid(row=0, column=0, padx=PADDING, pady=PADDING, sticky="ew")
+        
+        for i in range(10):
+            input_panel.grid_rowconfigure(i, weight=1)
+        for i in range(2):
+            input_panel.grid_columnconfigure(i, weight=1)
+            
+        # Create widgets in control panel
+        Label(input_panel, text="Budget Tracker", bg=CONTROL_BG_COLOR, fg=FG_COLOR).grid(row=0, column=0, columnspan=2, padx=PADDING)
+        Label(input_panel, text="Date:", bg=CONTROL_BG_COLOR, fg=FG_COLOR).grid(row=1, column=0, sticky="w")
+        Entry(input_panel, textvariable=self.date_var, bg=FG_COLOR, fg=CONTROL_BG_COLOR).grid(row=1, column=1, padx=(0, PADDING), pady=PADDING, sticky="ew")
+        Label(input_panel, text="Program:", bg=CONTROL_BG_COLOR, fg=FG_COLOR).grid(row=2, column=0, sticky="w")
+        Entry(input_panel, textvariable=self.transaction_type_var, bg=FG_COLOR, fg=CONTROL_BG_COLOR).grid(row=2, column=1, padx=(0, PADDING), pady=PADDING, sticky="ew")
+        
+        
+        # Crete control panel inside input panel
+        control_panel = Frame(input_control_panel, bg="#0000ff")
+        control_panel.grid(row=1, column=0, padx=PADDING, pady=PADDING)
+        
+        Label(control_panel, text="Category:", bg=CONTROL_BG_COLOR, fg=FG_COLOR).pack()
+        Label(control_panel, text="Category:", bg=CONTROL_BG_COLOR, fg=FG_COLOR).pack()
+        Label(control_panel, text="Category:", bg=CONTROL_BG_COLOR, fg=FG_COLOR).pack()
+        Label(control_panel, text="Category:", bg=CONTROL_BG_COLOR, fg=FG_COLOR).pack()
+        Entry(control_panel, textvariable=self.category_var, bg=FG_COLOR, fg=CONTROL_BG_COLOR).pack(padx=(0, PADDING), pady=PADDING, fill="x")
+        
+
+        
+        
 
 
 if __name__ == "__main__":
